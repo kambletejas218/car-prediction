586d07a9288469235478ecbfe4b9a3cce32a2567
Tejas Kamble <149971737+kambletejas218@users.noreply.github.com>
2024-11-15 15:09:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('CAR PREDICTION')
 st.write("This model predicts car price using below features.")
 
-with st.expander(Data):
+with st.expander('Data'):
   st.write("**RAW DATA**")
   df=pd.read_csv('https://raw.githubusercontent.com/kambletejas218/car-prediction/refs/heads/main/car_prediction_cleaned.csv')
   df
