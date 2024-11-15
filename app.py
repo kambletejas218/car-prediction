372e737edf6f82755c923c1638903c925388dded
Tejas Kamble <149971737+kambletejas218@users.noreply.github.com>
2024-11-15 14:56:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title('CAR PREDICTION')
 st.write("This model predicts car price using below features.")
 
-df=pd.read_csv('car_prediction_cleaned.csv')
+df=pd.read_csv('https://github.com/kambletejas218/car-prediction/blob/main/car_prediction_cleaned.csv')
 df.head()
